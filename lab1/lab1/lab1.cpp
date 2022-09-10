d916c8e079520742649b62e177e45cb1993323a6
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-09-10 23:05:18 +0300

    lab1 done

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string inputStr(string path)
+{
+
+    ifstream fin(path);
+    string tempStr;
+    string inputStr = "";
+    if (fin.is_open())
+    {
+        while (getline(fin, tempStr))
+        {
+            inputStr += tempStr;
+        }
+    }
+    fin.close();
+    return inputStr;
+}
+
+void OutputStr(string path, string result)
+{
+
+    ofstream fout(path);
+    if (fout.is_open())
+    {
+        fout << result;
+    }
+    fout.close();
+
+}
+
+string decode(string word)
+{
+    if (word.size() > 2)
+    {
+        string result = word.substr(word.length() - 3, 3) + word;
+        result = result.replace(result.length() - 3, 3, 0, ' ');
+        return result;
+    }
+    else
+    {
+        reverse(word.begin(), word.end());
+        return word;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    /*string path;
+    cin >> path;*/
+
+    string text = inputStr("input.txt");
+    string result = decode(text);
+    OutputStr("output.txt", result);
+
+
+}
