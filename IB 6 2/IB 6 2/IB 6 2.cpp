6a7c5897b4b3e9853c4077ca14c0a8aa8cfcf4b7
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2022-10-13 20:10:45 +0300

    6.2 final

--- IB 6 2/IB 6 2/IB 6 2.cpp
+++ IB 6 2/IB 6 2/IB 6 2.cpp
@@ -23,12 +23,12 @@ bool CheckSystem(vector<vector<bool>> a, vector<int> ls, vector<int> lo)
 	return true;
 }
 
-bool GetVectorsForSystem(const vector<vector<bool>>& a, vector<int>& ls, vector<int>& lo) 
+bool GetVectorsForSystem(const vector<vector<bool>>& a, vector<int>& ls, vector<int>& lo)
 {
 	ls.clear();
 	lo.clear();
 
-	for (int i = 0; i < a.size(); i++) 
+	for (int i = 0; i < a.size(); i++)
 	{
 		int cnt_s = 1;
 
@@ -37,32 +37,125 @@ bool GetVectorsForSystem(const vector<vector<bool>>& a, vector<int>& ls, vector<
 			if (a[i][j])
 			{
 				cnt_s++;
-			}				
+			}
 		}
 
 		ls.push_back(cnt_s);
 	}
 
-	for (int i = 0; i < a[0].size(); i++) 
+	for (int i = 0; i < a[0].size(); i++)
 	{
-		int cnt_o = 1;
+		int min = 1;
 
 		for (int j = 0; j < a.size(); j++)
 		{
-			if (!a[j][i])
+			if (!a[j][i] && ls[j] > min)
 			{
-				cnt_o++;
+				min = ls[j];
 			}
 		}
-		lo.push_back(cnt_o);
+		lo.push_back(min);
 	}
 
 	return CheckSystem(a, ls, lo);
 }
 
+void CreateSystem(vector<vector<bool>>& a, vector<int>& ls, vector<int>& lo)
+{
+
+	for (int i = 0; i < ls.size(); i++)
+	{
+		ls[i] = rand() % 10 + 1;
+	}
+	for (int i = 0; i < lo.size(); i++)
+	{
+		lo[i] = rand() % 10 + 1;
+	}
+	// w = true r = false
+	for (int i = 0; i < ls.size(); i++)
+	{
+		for (int j = 0; j < lo.size(); j++)
+		{
+			if (ls[i] > lo[j])
+			{
+				a[i][j] = true;
+			}
+			else
+			{
+				a[i][j] = false;
+			}
+		}
+	}
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<vector<bool>> matrixA;
+	int n, m;
+	cin >> n >> m;
+	matrixA.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		matrixA[i].resize(m);
+	}
+	vector<int> ls;
+	ls.resize(n);
+	vector<int> lo;
+	lo.resize(m);
+	CreateSystem(matrixA, ls, lo);
+	for (int i = 0; i < ls.size(); i++)
+	{
+		cout << ls[i] << " ";
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < lo.size(); i++)
+	{
+		cout << lo[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < ls.size(); i++)
+	{
+		for (int j = 0; j < lo.size(); j++)
+		{
+			if (matrixA[i][j])
+			{
+				cout << "W";
+			}
+			else
+			{
+				cout << "R";
+			}
+		}
+		cout << endl;
+	}
+	if (CheckSystem(matrixA, ls, lo))
+	{
+		cout << "SYSTEM SECURE" << endl;
+	}
+	else
+	{
+		cout << "SYSTEM UNSECURED" << endl;
+	}
+	if (GetVectorsForSystem(matrixA, ls, lo))
+	{
+		cout << "SECURE SYSTEM EXISTS VECTORS: " << endl;
+
+		for (int i = 0; i < ls.size(); i++)
+		{
+			cout << ls[i] << " ";
+		}
+
+		cout << endl;
+
+		for (int i = 0; i < lo.size(); i++)
+		{
+			cout << lo[i] << " ";
+		}
+	}
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
